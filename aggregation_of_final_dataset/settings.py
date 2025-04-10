8a347aca899ff500cfc90c35a59d696c15261624
filippovarini <fppvrn@gmail.com>
2025-04-10 10:42:02 +0000

    refactor noaa_puget to use utils.py functions

--- aggregation_of_final_dataset/settings.py
+++ aggregation_of_final_dataset/settings.py
@@ -6,4 +6,11 @@ class Settings:
     base_dir: Path = Path("/mnt/data/dev/fish-datasets/final_dataset")
     train_dataset_suffix: str = "_train"
     val_dataset_suffix: str = "_val"
+    images_folder_name: str = "JPEGImages"
+    coco_file_name: str = "coco_annotations.json"
+
+    # We only use one category for the fish
+    coco_category_id: int = 1
+    coco_categories = [{"id": coco_category_id, "name": "fish"}]
+    
 
--- data_preview/utils.py
+++ data_preview/utils.py
@@ -1,5 +1,9 @@
 import requests
 import zipfile
+import random
+from pathlib import Path
+
+import supervision as sv
 
 
 def download_file(url, save_path):
@@ -18,6 +22,8 @@ def extract_zip(zip_path, extract_to):
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(extract_to)
     print(f"Extraction complete: {zip_path}")
+    print("Removing Zipped files...")
+    zip_path.unlink()
     
     
 def download_and_extract_zip(data_dir, data_url, dataset_shortname):
@@ -32,3 +38,40 @@ def download_and_extract_zip(data_dir, data_url, dataset_shortname):
         print("Removing Zipped files...")
         data_path.unlink()
     return data_dir
+
+
+def visualize_supervision_dataset(dataset, num_samples=16, grid_size=(4, 4), size=(20, 12)):
+    """Visualize random samples from a dataset with bounding boxes and labels."""
+    print(f"Dataset length: {len(dataset)}")
+    print(f"Dataset classes: {dataset.classes}")
+    
+    box_annotator = sv.BoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
+
+    image_example = None
+    annotated_images = []
+    image_names = []
+
+    for _ in range(num_samples):
+        i = random.randint(0, len(dataset) - 1)  # Avoid index out of range
+
+        image_path, image, annotations = dataset[i]
+        labels = [dataset.classes[class_id] for class_id in annotations.class_id]
+
+        # Get image name
+        image_name = Path(image_path).stem
+        image_names.append(image_name)
+        
+        annotated_image = image.copy()
+        annotated_image = box_annotator.annotate(annotated_image, annotations)
+        annotated_image = label_annotator.annotate(annotated_image, annotations, labels)
+        annotated_images.append(annotated_image)
+
+        if len(annotations) > 0 and image_example is None:
+            image_example = annotated_image
+
+    sv.plot_images_grid(
+        annotated_images, grid_size=grid_size, titles=image_names, size=size, cmap="gray"
+    )
+
+    return image_example
