bd33550d12e21acd8351ae74ab025f4fd186e646
filippovarini <fppvrn@gmail.com>
2025-05-17 16:41:26 +0000

    complete visualization

--- data_preview/visualize_mit_river_herring.py
+++ data_preview/visualize_mit_river_herring.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+
 from data_preview.utils import (
-    download_and_extract
+    download_and_extract,
+    build_and_visualize_supervision_dataset_from_coco_dataset
 )
 
 
 DATASET_SHORTNAME = "mit_river_herring"
-DATA_DIR = Path("/mnt/data/dev/fish-datasets/data/raw") / DATASET_SHORTNAME
+DATA_DIR = Path("data/raw") / DATASET_SHORTNAME
 
 def download_data(data_dir: Path):
     if data_dir.exists():
@@ -17,13 +20,24 @@ def download_data(data_dir: Path):
 
     data_url = "https://storage.googleapis.com/public-datasets-lila/mit-river-herring/mit_river_herring.zip"
 
-    data_path_zip = data_dir / "images.zip"
-    
     print("Extracting data...")
     download_and_extract(data_dir, data_url, DATASET_SHORTNAME)
+    
+    annotations_path = data_dir / "mit_river_herring" / "mit_sea_grant_river_herring.json"
+    images_path = data_dir / "mit_river_herring"
+    
+    return annotations_path, images_path
+
 
 def main():
-    download_data(DATA_DIR)
+    annotations_path, images_path = download_data(DATA_DIR)
+    
+    image_example = build_and_visualize_supervision_dataset_from_coco_dataset(
+        images_dir=images_path,
+        annotations_path=annotations_path,
+    )
+    plt.imsave(f"data_preview/{DATASET_SHORTNAME}_sample_image.png", image_example)
+    
 
 if __name__ == "__main__":
     main()
